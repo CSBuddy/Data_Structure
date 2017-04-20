2f2b02477f9f01483d2be32b280eb32f1b76e8ec
Saish Mhatre <lambocrypt@users.noreply.github.com>
2017-04-20 19:53:11 +0530

    Add files via upload

---
+++ Linked List/internal_ll.c
@@ -0,0 +1,47 @@
+#include<stdlib.h>
+#include<stdio.h>
+//Below code use to create structure of node
+struct node
+{
+//Node contains 2 parts ,data & link part	
+	int data;
+	struct node* link;
+};
+//These nodes currently not pointing to any other so we assigning it to NULL
+	struct node* head=NULL;
+	struct node* first=NULL;
+	struct node* second=NULL;
+	
+int main(){
+	
+//Assigning memory in heap
+	first=(struct node*)malloc(sizeof(struct node));
+	second=(struct node*)malloc(sizeof(struct node));
+
+//Head points to first node	
+	head=first;
+//2 is stored in data part of first node	
+	first->data=2;
+//First points to second node	
+	first->link=second;
+//4 is stored in data part of second node
+	second->data=4;
+//Second node points to null
+	second->link=NULL;
+
+	printf("Linklist is\n");
+	printf("%d\n",first->data);
+ 	printf("%d\n",second->data);
+	printf("======================\n");	
+	printf("%d\n",head);
+	printf("%d\n",head->data);
+	printf("%d\n",head->link);
+	printf("%d\n",first);
+	printf("%d\n",first->link);
+	printf("%d\n",second);	
+	printf("%d\n",second->data);
+	printf("%d\n",second->link);
+return 0;
+
+
+}
