364e1114541f7ef562358f2a2886a750948254c9
Saish Mhatre <lambocrypt@users.noreply.github.com>
2017-05-06 20:04:04 +0530

    Add files via upload

---
+++ gcd.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int gcd(int x , int y)
+{
+	if(y==0)
+	{	   
+	   return x;
+	}
+	else
+	{
+	   gcd(y,x%y);
+	}
+}//gcd function ends here
+
+int main(){
+	int x,y,g;
+	printf("Enter X & Y\n");
+	scanf("%d%d", &x, &y);
+//Function calling
+	g=gcd(x,y);
+//If g is negative we will convert it into positive	
+	if(g<0)
+	{
+	  g*=-1;
+	}
+//This is the output of gcd of two numbers
+	printf("gcd(%d,%d)=%d\n",x,y,g);
+	return 0;
+}//main ends here
+
