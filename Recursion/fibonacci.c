19c649599e32f159fa9ec918da1dde416b7bc81d
har6451 <68863245+har6451@users.noreply.github.com>
2020-10-02 09:27:06 +0545

    Update fibonacci.c

--- Recursion/fibonacci.c
+++ Recursion/fibonacci.c
@@ -6,23 +6,22 @@ you can do it with iteration also*/
 //fibonacci function
 int fibo(int n)
 {
-	if(n==0 || n==1)
+	if(n==0 || n==1)     // base condition
 	{
 	  return n;
 	}
 	else
 	{
-	  return fibo(n-1)+fibo(n-2);
+	  return fibo(n-1)+fibo(n-2);  // recursive function
 	}
 }
 
-//main function
 int main()
 {
 	int n,f;
-	printf("Enter the n: ");
+	printf("Enter the number: ");
 	scanf("%d",&n);
 	f=fibo(n);
-	printf("Fibonacci:\n%d\n",f);
+	printf(" The Fibonacci Series is :\n%d\n",f);
 	return 0;
 }
